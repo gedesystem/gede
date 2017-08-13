35eac426d457a393f1c3d44c7a1ec8fd10e1c9fd
Filipe Boaventura <fmbboaventura@gmail.com>
2017-08-13 02:11:35 -0300

    Adicionando o tipo do usuario na variavel session.

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $senha = $_POST['senha'];
 
 conexao();
 
-$sql = "SELECT usuario, senha, nome FROM gede_usuarios WHERE usuario='$login' AND senha='$senha'";
+$sql = "SELECT usuario, senha, nome, tipo FROM gede_usuarios WHERE usuario='$login' AND senha='$senha'";
 
 $result = mysql_query($sql);
 
@@ -17,6 +17,7 @@ if (mysql_num_rows($result) > 0) {
     $_SESSION['login'] = $login;
     $_SESSION['senha'] = $senha;
     $_SESSION['nome'] = mysql_fetch_row($result)[2];
+    $_SESSION['tipo'] = mysql_fetch_row($result)[3];
     header('location:home.php');
 } else {
     unset($_SESSION['login']);
