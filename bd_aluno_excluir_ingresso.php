9d930d0f9b0c80699bc3fc0bf5a346f7f2c74438
Gerenciador de Dados Educacionais <gedeproject@gmail.com>
2017-08-14 20:42:03 -0300

    up
    ...

---
+++ bd_aluno_excluir_ingresso.php
@@ -0,0 +1,24 @@
+<?php
+require_once('funcoes_uteis.php');
+
+error_reporting(E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
+
+$Id = $_POST["id"];
+
+//Isset dos atributos
+//$exemplo = isset($_POST["exemplo"]);
+
+
+conexao();
+
+//Criar o comando sql
+$sql_atualiza = "";
+
+mysql_query($sql_atualiza) or die("Não foi possivel atualizar:  " . mysql_error());
+
+echo '<h2 color: #6d7679>Registro atualizado e salvo com sucesso!</h2>';
+echo '<h3 color: #6d7679>Redirecionando em 3 segundos...</h3>';
+
+//Criar o caminho do redirecionamento 
+header("refresh: 2; url=COLOCAR O CAMINHO AQU");
+?>
