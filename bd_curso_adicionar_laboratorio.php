ff5e9eb3929dbc2d4c656005799bfafdb42184c5
felipelfb <felipelfbarros@gmail.com>
2017-08-20 13:57:18 -0300

    Classes da primeira imagem do documento de metas.

--- bd_curso_adicionar_laboratorio.php
+++ bd_curso_adicionar_laboratorio.php
@@ -8,16 +8,19 @@ $Id = $_POST["id"];
 //Isset dos atributos
 //$exemplo = isset($_POST["exemplo"]);
 
+$codigo_laboratorio = isset($_POST["nCodigo"]);
+$observacoes = isset($_POST["nObservacao"]);
 
 conexao();
 
 //Criar o comando sql
-$sql_atualiza = "";
+$sql_atualiza = "INSERT INTO cursos_laboratorio (codigo_curso, codigo_laboratorio, observacoes) VALUES ('" .$Id . "', "
+        . "'" . $codigo_laboratorio . "', '" . $observacoes . "')";
 
-mysql_query($sql_atualiza) or die("Não foi possivel atualizar:  " . mysql_error());
+mysql_query($sql_atualiza) or die("Não foi possivel adicionar:  " . mysql_error());
 
-echo '<h2 color: #6d7679>Registro atualizado e salvo com sucesso!</h2>';
-echo '<h3 color: #6d7679>Redirecionando em 3 segundos...</h3>';
+echo '<h2 color: #6d7679>Registro adicionado e salvo com sucesso!</h2>';
+echo '<h3 color: #6d7679>Redirecionando em 2 segundos...</h3>';
 
 //Criar o caminho do redirecionamento 
 header("refresh: 2; url=COLOCAR O CAMINHO AQU");
