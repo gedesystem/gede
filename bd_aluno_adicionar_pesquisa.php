35e4de1e67550c6263a73fb965eb2b7580f8ab79
felipelfb <felipelfbarros@gmail.com>
2017-08-20 16:45:35 -0300

    Classes da segunda imagem do documento de metas.

--- bd_aluno_adicionar_pesquisa.php
+++ bd_aluno_adicionar_pesquisa.php
@@ -3,22 +3,32 @@ require_once('funcoes_uteis.php');
 
 error_reporting(E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
 
-$Id = $_POST["id"];
+$matricula_uefs = $_POST["id"];
 
 //Isset dos atributos
 //$exemplo = isset($_POST["exemplo"]);
 
+$remuneracao = isset($_POST["nRemuneracao"]);
+$orientador = isset($_POST["nOrientador"]);
+$titulo_plano_trabalho = isset($_POST["nTituloPlano"]);
+$titulo_projeto = isset($_POST["nTituloProjeto"]);
+$modalidade = isset($_POST["nModalidade"]);
+$inicio = isset($_POST["nInicio"]);
+$fim = isset($_POST["nFim"]);
+$observacao = isset($_POST["nObservacao"]) ? $_POST["nObservacao"] : "";
 
 conexao();
 
 //Criar o comando sql
-$sql_atualiza = "";
+$sql_atualiza = "INSERT INTO aluno_pesquisa (matricula_uefs, remuneracao, orientador, titulo_plano_trabalho, titulo_projeto, inicio, fim, observacao) "
+        . "VALUES ('" . $matricula_uefs . "', '" . $remuneracao . "', '" . $orientador . "', '" . $titulo_plano_trabalho . "', '" . $titulo_projeto . "', '" . $modalidade . "', "
+        . "'" . $inicio . "', '" . $fim . "', '" . $observacao . "')";
 
-mysql_query($sql_atualiza) or die("Não foi possivel atualizar:  " . mysql_error());
+mysql_query($sql_atualiza) or die("Não foi possivel adicionar:  " . mysql_error());
 
-echo '<h2 color: #6d7679>Registro atualizado e salvo com sucesso!</h2>';
-echo '<h3 color: #6d7679>Redirecionando em 3 segundos...</h3>';
+echo '<h2 color: #6d7679>Registro adicionado e salvo com sucesso!</h2>';
+echo '<h3 color: #6d7679>Redirecionando em 2 segundos...</h3>';
 
 //Criar o caminho do redirecionamento 
-header("refresh: 2; url=COLOCAR O CAMINHO AQU");
+header("refresh: 2; url=modulo_alunos.php");
 ?>
